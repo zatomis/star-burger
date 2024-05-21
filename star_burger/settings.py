1d0002515ba99823bb571c3418dff64d419d1d8e
zatomis <zatomis@list.ru>
2024-05-21 13:54:11 +0300

    Bug fix

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -109,14 +109,6 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-#     )
-# }
-
-
-# psycopg2
 DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
 }
