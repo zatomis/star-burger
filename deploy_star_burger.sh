4eb3a0a75d9ab21f607a27ac8b1edcd73a8ed7ab
zatomis <zatomis@list.ru>
2024-05-24 18:17:18 +0300

    Bug fix

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -6,7 +6,6 @@ git pull
 echo 'Создание среды окружения Python'
 python3 -m venv venv
 source venv/bin/activate
-sleep 1
 echo 'Установка зависимостей '
 pip install -r requirements.txt
 pip install django-extensions
@@ -14,7 +13,7 @@ pip install rollbar
 clear
 echo 'Установка NodeJS '
 npm ci --dev
-./node_modules/.bin/parcel bundles-src/index.js --dist-dir bundles --public-url="./"
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 echo 'Сбор статики '
 python3 manage.py collectstatic --noinput
 echo 'Выполнение миграции '
@@ -29,17 +28,18 @@ else
 	echo -n "Отсутствует файл для работы сервиса"$'\n'$FILE$'\n'"Необходимо создать. Используйте Readme проекта"
 fi
 
-FILE=/etc/systemd/system/nginx.service
-if test -f "$FILE"; then
-	sudo systemctl restart /etc/systemd/system/nginx.service
+FILE1=/etc/systemd/system/nginx.service
+if test -f "$FILE1"; then
+	sudo systemctl reload nginx.service
 else
-	echo -n "Отсутствует NGINX сервис"$'\n'$FILE$'\n'"Необходимо установить"
+	clear
+	echo -n "Отсутствует NGINX сервис"$'\n'$FILE1$'\n'"Необходимо установить"
 fi
 
 FILE=/opt/star-burger/.env
 if test -f "$FILE"; then
-	source .env
 	cd /opt/star-burger
+	source .env
 	git_id="$(git rev-parse --verify HEAD)"
 	user="$(whoami)"
 	echo $ROLLBAR_TOKEN_KEY
@@ -53,7 +53,7 @@ if test -f "$FILE"; then
 	  "rollbar_username": "'user'",
 	  "environment": "PyCharm",
 	  "revision": "1",
-	  "local_username": "Max",
+	  "local_username": "'user'",
 	  "comment": "Comment git: '$git_id'",
 	  "status": "succeeded"
 	}'
